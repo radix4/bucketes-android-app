d3da9d7d7d560a4e35c392f73ab9540dd400731c
Thang Cao <shiyoline@gmail.com>
2021-03-17 11:00:56 -0700

    display menu to main_activity

--- app/src/main/java/com/example/bucketes/MainActivity.java
+++ app/src/main/java/com/example/bucketes/MainActivity.java
@@ -5,6 +5,8 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -29,4 +31,23 @@ public class MainActivity extends AppCompatActivity {
         mainRecyclerView.setAdapter(mainAdapter);
         mainRecyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
+
+
+
+    /**
+     * This method displays the menu to the main_activity
+     * */
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
 }
+
+
+/** Notes:
+ *  1. MainRecyclerViewAdapter is necessary to display the list to the main menu.
+ *
+ *
+ * */
