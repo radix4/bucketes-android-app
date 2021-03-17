348a3d8d20d9166a08eb3f57853937c3ee97b84f
Thang Cao <shiyoline@gmail.com>
2021-03-17 10:51:20 -0700

    static display of items to the main activity

--- app/src/main/java/com/example/bucketes/MainActivity.java
+++ app/src/main/java/com/example/bucketes/MainActivity.java
@@ -1,14 +1,32 @@
 package com.example.bucketes;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    RecyclerView mainRecyclerView;
+
+    String[] items, descriptions;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        mainRecyclerView = findViewById(R.id.mainRecyclerView);
+
+        items = getResources().getStringArray(R.array.items);
+        descriptions = getResources().getStringArray(R.array.descriptions);
+
+
+        MainRecyclerViewAdapter mainAdapter = new MainRecyclerViewAdapter(this, items, descriptions);
+        mainRecyclerView.setAdapter(mainAdapter);
+        mainRecyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
 }
