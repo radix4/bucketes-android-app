facd10882f4d783ff998e08ffd3256753ef3e4a9
Thang Cao <shiyoline@gmail.com>
2021-03-25 10:02:47 -0700

    Add items_table to the database

--- app/src/main/java/com/example/bucketes/DBHelper.java
+++ app/src/main/java/com/example/bucketes/DBHelper.java
@@ -12,29 +12,61 @@ public class DBHelper extends SQLiteOpenHelper {
     public static final String USERS_TABLE = "users_table";
     public static final String COL_USERNAME = "username";
     public static final String COL_PASSWORD = "password";
+    public static final String COL_ITEM_ID = "item_id";
+    public static final String COL_TITLE = "title";
+    public static final String COL_STORY = "story";
+    public static final String COL_COMPLETION_DATE = "completion_date";
+    public static final String COL_STATUS = "status";
 
+    /**
+     * This function creates the database if the database has not been created.
+     * */
     public DBHelper(Context context) {
         super(context, DB_NAME, null, 1);
     }
 
-    // this is called the first time a database is accessed
-    // this creates a new database
+    /**
+     * This function creates database tables.
+     * This function is called the first time a database is accessed.
+     * */
     @Override
     public void onCreate(SQLiteDatabase MyDB) {
-        String createStatementTable = "create table " + USERS_TABLE + " (" + COL_USERNAME + " text primary key, " + COL_PASSWORD + " text)";
+        String createUsersTableStatement =
+                "create table " + USERS_TABLE + " (" +
+                        COL_USERNAME + " text primary key, " +
+                        COL_PASSWORD + " text)";
 
-        MyDB.execSQL(createStatementTable);
+        String createItemsTableStatement =
+                "create table items_table (" +
+                        COL_ITEM_ID + " integer, " +
+                        COL_USERNAME + " text, " +
+                        COL_TITLE + " text, " +
+                        COL_STORY + " text, " +
+                        COL_COMPLETION_DATE + " text, " +
+                        COL_STATUS + " text, " +
+                        "primary key (" + COL_ITEM_ID + "), " +
+                        "foreign key (" + COL_USERNAME + ") references " + USERS_TABLE;
+
+
+        /* create 2 tables into the db */
+        MyDB.execSQL(createUsersTableStatement);
+        MyDB.execSQL(createItemsTableStatement);
     }
 
+    // TODO: add comments
     @Override
     public void onUpgrade(SQLiteDatabase MyDB, int i, int i1) {
         MyDB.execSQL("drop Table if exists users");
     }
 
+
+    /**
+     * This function inserts the UserModel into the database.
+     * */
     public boolean addUser(UserModel userModel) {
         SQLiteDatabase db = this.getWritableDatabase();
 
-        ContentValues cv = new ContentValues(); // this works similar to a hashmap with keys and values
+        ContentValues cv = new ContentValues(); // ContentValues works similar to a hashmap with keys and values
         cv.put(COL_USERNAME, userModel.getUsername());
         cv.put(COL_PASSWORD, userModel.getPassword());
 
@@ -46,9 +78,12 @@ public class DBHelper extends SQLiteOpenHelper {
             return false;
         else
             return true;
-
     }
 
+    /**
+     * This function validates if the UserModel entered by the user matches with the
+     * data in the database.
+     * */
     public boolean validateUser(UserModel userModel) {
         SQLiteDatabase db = this.getReadableDatabase();
 
