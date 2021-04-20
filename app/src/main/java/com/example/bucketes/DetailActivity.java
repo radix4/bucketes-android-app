f04ba2de0ee8061e1265e153f3e326269e0f55be
George Michael Cuevas <george.cuevas-garcia@sjsu.edu>
2021-04-19 19:27:34 -0700

    Update DetailActivity.java

--- app/src/main/java/com/example/bucketes/DetailActivity.java
+++ app/src/main/java/com/example/bucketes/DetailActivity.java
@@ -1,23 +1,31 @@
 package com.example.bucketes;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.bucketes.dialogs.AddItemDialog;
 import com.example.bucketes.dialogs.LogoutDialog;
 import com.example.bucketes.dialogs.SaveDialog;
+import com.example.bucketes.models.Item;
+import com.example.bucketes.models.User;
 
 public class DetailActivity extends AppCompatActivity implements SaveDialog.CustomDialogListener {
 
     RadioGroup radioGroup;
     RadioButton radioButton;
+
+    private EditText item;
+    private Button save;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +33,8 @@ public class DetailActivity extends AppCompatActivity implements SaveDialog.Cust
         setContentView(R.layout.detailed_item);
 
         radioGroup = findViewById(R.id.radioGroup);
+        EditText title = findViewById(R.id.editTitle);
+        EditText story = findViewById(R.id.editTitle);
         Button cancel = findViewById(R.id.button1);
         Button save = findViewById(R.id.button2);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
@@ -36,10 +46,19 @@ public class DetailActivity extends AppCompatActivity implements SaveDialog.Cust
                 openSaveDialog();
             }
         });
+
         save.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                DBHelper dbHelper = new DBHelper(DetailActivity.this);
+                Item title;
+                User user;
 
+                // attempt to instantiate title
+                //title = new Item(title.getText().toString(), story.getText().toString());
+                //if (title.matches("")) {
+                //    Toast.makeText(DetailActivity.this, "Error adding title", Toast.LENGTH_SHORT).show();
+                //}
             }
         });
     }
@@ -55,4 +74,5 @@ public class DetailActivity extends AppCompatActivity implements SaveDialog.Cust
         Intent intent = new Intent(DetailActivity.this, MainActivity.class);
         startActivity(intent);
     }
+
 }
